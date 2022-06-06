920dcb5c30743c764f39b731bc5c52147cb709c0
victorkiambi <kiambi15@gmail.com>
2022-06-06 21:15:43 +0300

    bug fix

--- forms/contact.php
+++ forms/contact.php
@@ -25,12 +25,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
 
-  $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'kiambi15@gmail.com',
-    'password' => 'paswordgoogle',
-    'port' => '587'
-  );
+  // $contact->smtp = array(
+  //   'host' => 'smtp.gmail.com',
+  //   'username' => 'kiambi15@gmail.com',
+  //   'password' => 'paswordgoogle',
+  //   'port' => '587'
+  // );
   
 
   $contact->add_message( $_POST['name'], 'From');
